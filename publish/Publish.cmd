f5d9046413b1829271d34234312d8b706c6b2d48
dirk <dirk@git.imagemagick.org>
2016-07-27 00:03:41 +0200

    Published QRCodeCore.1.0.0-alpha2.

---
+++ publish/Publish.cmd
@@ -0,0 +1,8 @@
+@echo off
+call "%vs140comntools%vsvars32.bat"
+
+msbuild "..\QRCodeCore.sln" /m /p:Configuration=Release
+dotnet test ..\tests\QRCodeCore.Tests\project.json
+dotnet pack --output . --no-build --configuration Release --version-suffix alpha2 ..\src\QRCodeCore\project.json
+
+del *.symbols.nupkg
