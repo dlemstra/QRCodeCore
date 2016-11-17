1e656b350f5482311663f369f9eb11b3e81a6cb7
Dirk Lemstra <dirk@git.imagemagick.org>
2016-11-18 00:38:37 +0100

    Added exit code checks.

--- publish/Publish.cmd
+++ publish/Publish.cmd
@@ -2,7 +2,19 @@
 call "%vs140comntools%vsvars32.bat"
 
 msbuild "..\QRCodeCore.sln" /m /p:Configuration=Release
+if %ERRORLEVEL% neq 0 goto failed
+
 dotnet test ..\tests\QRCodeCore.Tests\project.json
+if %ERRORLEVEL% neq 0 goto failed
+
 dotnet pack --output . --no-build --configuration Release --version-suffix alpha4 ..\src\QRCodeCore\project.json
+if %ERRORLEVEL% neq 0 goto failed
 
 del *.symbols.nupkg
+
+goto done:
+
+:failed:
+pause
+
+:done
