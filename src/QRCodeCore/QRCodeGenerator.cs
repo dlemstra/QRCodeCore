abd5907c553f575dcc7d61ffdb49034ffdf205f1
Dirk Lemstra <dirk@git.imagemagick.org>
2018-01-01 22:30:53 +0100

    Changed copyright year.

--- src/QRCodeCore/QRCodeGenerator.cs
+++ src/QRCodeCore/QRCodeGenerator.cs
@@ -1,5 +1,4 @@
-//=================================================================================================
-// Copyright 2016 Dirk Lemstra <https://github.com/dlemstra/QRCodeCore>
+// Copyright 2016-2018 Dirk Lemstra <https://github.com/dlemstra/QRCodeCore>
 //
 // Licensed under the Apache 2.0 License (the "License"); you may not use this file except in 
 // compliance with the License. You may obtain a copy of the License at
@@ -10,7 +9,6 @@
 // License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 // express or implied. See the License for the specific language governing permissions and
 // limitations under the License.
-//=================================================================================================
 
 using System;
 using System.Collections.Generic;
