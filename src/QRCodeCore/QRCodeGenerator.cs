239f5fbc4413e839700d0cfd3469c9aa6d949e04
Dirk Lemstra <dirk@lemstra.org>
2019-11-26 19:56:03 +0100

    Enabled nullable reference types.

--- src/QRCodeCore/QRCodeGenerator.cs
+++ src/QRCodeCore/QRCodeGenerator.cs
@@ -27,11 +27,11 @@ namespace QRCodeCore
         private int[] alignmentPatternBaseValues = { 0, 0, 0, 0, 0, 0, 0, 6, 18, 0, 0, 0, 0, 0, 6, 22, 0, 0, 0, 0, 0, 6, 26, 0, 0, 0, 0, 0, 6, 30, 0, 0, 0, 0, 0, 6, 34, 0, 0, 0, 0, 0, 6, 22, 38, 0, 0, 0, 0, 6, 24, 42, 0, 0, 0, 0, 6, 26, 46, 0, 0, 0, 0, 6, 28, 50, 0, 0, 0, 0, 6, 30, 54, 0, 0, 0, 0, 6, 32, 58, 0, 0, 0, 0, 6, 34, 62, 0, 0, 0, 0, 6, 26, 46, 66, 0, 0, 0, 6, 26, 48, 70, 0, 0, 0, 6, 26, 50, 74, 0, 0, 0, 6, 30, 54, 78, 0, 0, 0, 6, 30, 56, 82, 0, 0, 0, 6, 30, 58, 86, 0, 0, 0, 6, 34, 62, 90, 0, 0, 0, 6, 28, 50, 72, 94, 0, 0, 6, 26, 50, 74, 98, 0, 0, 6, 30, 54, 78, 102, 0, 0, 6, 28, 54, 80, 106, 0, 0, 6, 32, 58, 84, 110, 0, 0, 6, 30, 58, 86, 114, 0, 0, 6, 34, 62, 90, 118, 0, 0, 6, 26, 50, 74, 98, 122, 0, 6, 30, 54, 78, 102, 126, 0, 6, 26, 52, 78, 104, 130, 0, 6, 30, 56, 82, 108, 134, 0, 6, 34, 60, 86, 112, 138, 0, 6, 30, 58, 86, 114, 142, 0, 6, 34, 62, 90, 118, 146, 0, 6, 30, 54, 78, 102, 126, 150, 6, 24, 50, 76, 102, 128, 154, 6, 28, 54, 80, 106, 132, 158, 6, 32, 58, 84, 110, 136, 162, 6, 26, 54, 82, 110, 138, 166, 6, 30, 58, 86, 114, 142, 170 };
         private int[] remainderBits = { 0, 7, 7, 7, 7, 7, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0 };
 
-        private List<AlignmentPattern> alignmentPatternTable;
-        private List<ECCInfo> capacityECCTable;
-        private List<VersionInfo> capacityTable;
-        private List<Antilog> galoisField;
-        private Dictionary<char, int> alphanumEncDict;
+        private readonly List<AlignmentPattern> alignmentPatternTable = new List<AlignmentPattern>();
+        private readonly List<ECCInfo> capacityECCTable = new List<ECCInfo>();
+        private readonly List<VersionInfo> capacityTable = new List<VersionInfo>();
+        private readonly List<Antilog> galoisField = new List<Antilog>();
+        private readonly Dictionary<char, int> alphanumEncDict = new Dictionary<char, int>();
 
         public QRCodeGenerator()
         {
@@ -930,15 +930,12 @@ namespace QRCodeCore
 
         private void CreateAlphanumEncDict()
         {
-            this.alphanumEncDict = new Dictionary<char, int>();
             for (int i = 0; i < alphanumEncTable.Length; i++)
                 this.alphanumEncDict.Add(alphanumEncTable[i], i);
         }
 
         private void CreateAlignmentPatternTable()
         {
-            this.alignmentPatternTable = new List<AlignmentPattern>();
-
             for (var i = 0; i < (7 * 40); i += 7)
             {
                 var points = new List<Point>();
@@ -970,7 +967,6 @@ namespace QRCodeCore
 
         private void CreateCapacityECCTable()
         {
-            this.capacityECCTable = new List<ECCInfo>();
             for (var i = 0; i < (4 * 6 * 40); i = i + (4 * 6))
             {
                 this.capacityECCTable.AddRange(
@@ -1026,7 +1022,6 @@ namespace QRCodeCore
 
         private void CreateCapacityTable()
         {
-            this.capacityTable = new List<VersionInfo>();
             for (var i = 0; i < (16 * 40); i += 16)
             {
                 this.capacityTable.Add(new VersionInfo((i + 16) / 16,
@@ -1075,8 +1070,6 @@ namespace QRCodeCore
 
         private void CreateAntilogTable()
         {
-            this.galoisField = new List<Antilog>();
-
             for (var i = 0; i < 256; i++)
             {
                 var gfItem = (int)Math.Pow(2, i);
